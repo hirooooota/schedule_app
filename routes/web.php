52ca2cbdb15301bf9f02618c2e796be539c80fa4
hirooooota <hirooooota@gmail.com>
2023-09-09 18:23:47 +0900

    カレンダー表示の実装

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\FullCalendarController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,4 +27,9 @@ Route::middleware([
     Route::get('/', [EventController::class, 'index'])
         ->name('root');
     Route::resource('events', EventController::class);
+
+    Route::get('/calendar', function() {
+        return view('full-calendar');
+    })->name('calendar');
+    Route::get('/calendar/action', [FullCalendarController::class, 'index']);
 });
